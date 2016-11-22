cee5fbe71de7a6eb94af2d161da725055b054141
Gerwin <outsidewer@hotmail.com>
2016-11-22 13:45:33 +0100

    Add files via upload

---
+++ General Addons/camera.cpp
@@ -0,0 +1,23 @@
+/*
+	-- FPS Mode --
+*/
+	int GetPlayerCameraID(int playerid, int camid);
+//
+
+// -- Client check debug --
+//
+//for(new i= 0x00; i <= 0xff; i++) SendClientCheck(playerid, i, i, i, 0x4);
+//
+
+
+//
+/*
+
+	case 0x [               ]:
+		Log( " XXX " );
+	break;
+*/
+
+
+// -- For game files --
+native GetModuleHandle(file[]);
