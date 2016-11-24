796a40fc6c04b7e5821a118348a719886aff6d32
Gerwin <outsidewer@hotmail.com>
2016-11-24 13:03:03 +0100

    Create anticheat.cpp

---
+++ RakNet/anticheat.cpp
@@ -0,0 +1,50 @@
+	 // --
+  if (szBindAddress) {
+		printf( " Started Server on %s:%d, with maxplayers: %d lanmode: %s ac: %s.\n",
+			szBindAddress, dwPort, dwMaxPlayers, bLanMode?"ON":"OFF", antiCheat?"ON":"OFF" );
+	} else {
+		printf( " Started Server on port: %d, with maxplayers: %d lanmode: %s ac: %s.\n",
+			dwPort, dwMaxPlayers, bLanMode?"ON":"OFF", antiCheat?"ON":"OFF" );
+}
+
+// --
+/*
+		case ID_AC:
+			Packet_AntiCheat(p);
+break;
+*/
+
+
+// -- for OnPlayerReconnect -- 
+void CNetGame::Packet_NewIncomingConnection(Packet* packet)
+{
+    in_addr in;
+  /*
+  
+  */
+}  
+
+
+
+// -- Small fix --
+
+void CNetGame::AddBan(char * nick, char * ip_mask, char * reason)
+{
+	const struct tm *tm;
+	time_t now;
+	now = time(NULL);
+	tm = localtime(&now);
+	char *s;
+	s = new char[128];
+	strftime(s, 128, "[%d/%m/%y | %H:%M:%S]", tm);
+
+	m_pRak->AddToBanList(ip_mask);
+	
+	FILE * fileBanList = fopen("samp.ban","a");
+	if(!fileBanList) return;
+	
+	fprintf(fileBanList,"%s %s %s - %s\n", ip_mask, s, nick, reason);
+	fclose(fileBanList);
+
+	delete [] s;
+}
